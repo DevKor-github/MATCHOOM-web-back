fab61fff48e41b91943372bb6136bae4d5946626
liverforpresent <dailynsteady@gmail.com>
2024-11-28 02:09:59 +0900

    chore: 쿠키 설정 관련 패키지 설치 및 설정

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -29,6 +30,9 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  // cookie parser 사용
+  app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
 }
