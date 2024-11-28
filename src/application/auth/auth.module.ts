26a68211df803e02eba1a537d201b1da2e3060db
liverforpresent <dailynsteady@gmail.com>
2024-11-29 01:34:43 +0900

    chore: jwt-refresh strategy 모듈에 추가

--- src/application/auth/auth.module.ts
+++ src/application/auth/auth.module.ts
@@ -5,13 +5,14 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserService } from 'src/domain/user/user.service';
 import { User } from 'src/domain/user/entities/user.entity';
+import { JwtRefreshStrategy } from './strategies/jwtRefresh.strategy';
 
 @Module({
   imports:[
     TypeOrmModule.forFeature([User]),
     JwtModule.register({})
   ],
-  providers: [AuthService, UserService],
+  providers: [AuthService, UserService, JwtRefreshStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
