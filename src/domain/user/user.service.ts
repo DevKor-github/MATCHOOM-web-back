7d141104bb28b9de298d51439c4c4e0df5c264fa
liverforpresent <dailynsteady@gmail.com>
2024-11-28 23:18:40 +0900

    fix: 유저 전화번호 중복체크 추가

--- src/domain/user/user.service.ts
+++ src/domain/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
@@ -26,6 +26,8 @@ export class UserService {
     if (!user) throw new NotFoundException("존재하지 않는 사용자 입니다.");
 
     const { name, phone, bank, account } = updateUserDto;
+    const phoneCheck = this.userRepository.findOne({ where: { phone } });
+    if (phoneCheck) throw new ConflictException("중복된 전화번호 입니다.");
 
     user.name = name || user.name;
     user.phone = phone || user.phone;
