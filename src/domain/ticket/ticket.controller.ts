b744d294dccee2a784eaff670496de6ceb546c8c
liverforpresent <dailynsteady@gmail.com>
2024-11-30 20:34:46 +0900

    feat: ticket api 계획

--- src/domain/ticket/ticket.controller.ts
+++ src/domain/ticket/ticket.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { TicketService } from './ticket.service';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller('ticket')
-export class TicketController {}
+@Controller(':studio/ticket')
+@ApiTags('ticket')
+export class TicketController {
+  constructor(
+    private readonly ticketService: TicketService
+  ) { }
+
+  @Get(':ticketId')
+  async getTicketInfo(@Param('studio')studio: number, @Param('ticketId') TicketId: number) {
+    
+  }
+
+  @Post()
+  async createTicket(@Body() ticketCreateReqDto: any) {
+
+  }
+
+  @Patch(':ticketId')
+  async updateTicket(@Body() ticketUpdateReqDto: any, @Param('ticketId') TicketId: number) {
+    
+  }
+
+  @Delete(':ticketId')
+  async deleteTicket(@Param('ticketId') TicketId: number) {
+
+  }
+}
