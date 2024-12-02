087d2f3beef0283f754d7dabfcb6e5872d9b1a8d
Na1gae <nalgae@korea.ac.kr>
2024-12-02 19:55:07 +0900

    Chore: CreateLectureDto

--- src/domain/lecture/dtos/lecture.dto.ts
+++ src/domain/lecture/dtos/lecture.dto.ts
@@ -16,11 +16,11 @@ class CreateLectureDto{
     file: number
 
     @IsDateString()
-    @ApiProperty({example: "강의 시간 As DateString"})
-    date: Date
+    @ApiProperty({example: "강의 시간 As DateString[]"})
+    date: Date[]
 
     @IsString()
-    @ApiProperty({example: ""})
+    @ApiProperty({example: "강의 진행 위치"})
     room: string
 
     @IsNumber()
@@ -33,17 +33,20 @@ class CreateLectureDto{
     @ApiProperty({example: "0: Free 1: Basic 2: Middle 3: Professional"})
     difficulty: number
 
-    @IsBoolean()
+    @IsString()
+    @IsOptional()
     @ApiProperty({example: "true이면 원데이 false이면 정기강의"})
-    type: boolean
+    type: string
 
     @IsInt()
+    @IsOptional()
     @Max(10)
     @Min(0)
     @ApiProperty({example: "장르코드 / 0~10 중 하나만 number"})
     genre: number
 
     @IsString()
+    @IsOptional()
     @MaxLength(500, {message: "강의 설명은 500자 이하여야 합니다."})
     @ApiProperty({example: "강의설명설명설명"})
     description: string
