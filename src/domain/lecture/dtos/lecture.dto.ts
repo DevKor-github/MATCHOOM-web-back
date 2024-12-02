2228eb876f64b837334439ac6b4ea8e14dea3cdd
Na1gae <nalgae@korea.ac.kr>
2024-12-02 16:54:27 +0900

    Feat: Lecture Create/Delete Dto

---
+++ src/domain/lecture/dtos/lecture.dto.ts
@@ -0,0 +1,57 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsArray, IsBoolean, IsDateString, IsInt, IsNumber, IsOptional, IsString, Max, MaxLength, Min } from "class-validator";
+
+class CreateLectureDto{
+    @IsString()
+    @ApiProperty({example: "강의 이름"})
+    name: string
+
+    @IsString()
+    @ApiProperty({example: "강사명"})
+    instructor: string
+
+    @IsNumber()
+    @IsOptional()
+    @ApiProperty({example: "User에 연결된 File 테이블에서의 File id"})
+    file: number
+
+    @IsDateString()
+    @ApiProperty({example: "강의 시간 As DateString"})
+    date: Date
+
+    @IsString()
+    @ApiProperty({example: ""})
+    room: string
+
+    @IsNumber()
+    @ApiProperty({example:20000})
+    price: number
+
+    @IsInt()
+    @Max(3)
+    @Min(0)
+    @ApiProperty({example: "0: Free 1: Basic 2: Middle 3: Professional"})
+    difficulty: number
+
+    @IsBoolean()
+    @ApiProperty({example: "true이면 원데이 false이면 정기강의"})
+    type: boolean
+
+    @IsInt()
+    @Max(10)
+    @Min(0)
+    @ApiProperty({example: "장르코드 / 0~10 중 하나만 number"})
+    genre: number
+
+    @IsString()
+    @MaxLength(500, {message: "강의 설명은 500자 이하여야 합니다."})
+    @ApiProperty({example: "강의설명설명설명"})
+    description: string
+}
+
+class DeleteLectureDto{
+    @IsNumber()
+    lectureId: number
+}
+
+export { CreateLectureDto, DeleteLectureDto}
