72f21afb32722a8d091cdb088440080815373889
liverforpresent <dailynsteady@gmail.com>
2024-12-03 17:28:15 +0900

    fix: 쿠키 설정 변경

--- src/application/auth/configs/cookie.config.ts
+++ src/application/auth/configs/cookie.config.ts
@@ -2,29 +2,29 @@ import { CookieOptions } from "express";
 
 export const CookieConfig = {
   accessToken: {
-    httpOnly: true,
+    httpOnly: false,
     secure: false,
-    sameSite: 'none',
+    sameSite: 'lax',
     domain: process.env.CLIENT_DOMAIN,
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
   } as CookieOptions,
   refreshToken: {
-    httpOnly: true,
+    httpOnly: false,
     secure: false,
-    sameSite: 'none',
+    sameSite: 'lax',
     domain: process.env.CLIENT_DOMAIN,
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000 * 30),
   } as CookieOptions,
   onboardingToken: {
-    httpOnly: true,
+    httpOnly: false,
     secure: false,
-    sameSite: 'none',
+    sameSite: 'lax',
     domain: process.env.CLIENT_DOMAIN
   } as CookieOptions,
   tokenDelete: {
-    httpOnly: true,
+    httpOnly: false,
     secure: false,
-    sameSite: 'none',
+    sameSite: 'lax',
     domain: process.env.CLIENT_DOMAIN
   } as CookieOptions
 };
