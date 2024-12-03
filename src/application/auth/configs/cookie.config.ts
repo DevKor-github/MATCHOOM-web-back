d524e6a797976412757f7bc9c9f9e631d5b423e9
liverforpresent <dailynsteady@gmail.com>
2024-12-03 17:19:05 +0900

    fix: 쿠키 설정 변경

--- src/application/auth/configs/cookie.config.ts
+++ src/application/auth/configs/cookie.config.ts
@@ -4,27 +4,27 @@ export const CookieConfig = {
   accessToken: {
     httpOnly: true,
     secure: false,
-    sameSite: 'lax',
+    sameSite: 'none',
     domain: process.env.CLIENT_DOMAIN,
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
   } as CookieOptions,
   refreshToken: {
     httpOnly: true,
     secure: false,
-    sameSite: 'lax',
+    sameSite: 'none',
     domain: process.env.CLIENT_DOMAIN,
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000 * 30),
   } as CookieOptions,
   onboardingToken: {
     httpOnly: true,
     secure: false,
-    sameSite: 'lax',
+    sameSite: 'none',
     domain: process.env.CLIENT_DOMAIN
   } as CookieOptions,
   tokenDelete: {
     httpOnly: true,
     secure: false,
-    sameSite: 'lax',
+    sameSite: 'none',
     domain: process.env.CLIENT_DOMAIN
   } as CookieOptions
 };
