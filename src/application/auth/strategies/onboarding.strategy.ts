d9da9320e859dd8df4b8087d05f8f09f275d1ac7
liverforpresent <dailynsteady@gmail.com>
2024-12-03 20:06:06 +0900

    fix: 토큰 로직 수정

--- src/application/auth/strategies/onboarding.strategy.ts
+++ src/application/auth/strategies/onboarding.strategy.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { Request } from 'express';
 
 @Injectable()
 export class RegisterStrategy extends PassportStrategy(Strategy, 'register') {
@@ -22,7 +23,10 @@ export class RegisterStrategy extends PassportStrategy(Strategy, 'register') {
     });
   }
 
-  async validate(payload: JwtPayload) {
+  async validate(req: Request, payload: JwtPayload) {
+    const refreshToken = req.headers.authorization.split(' ')[1];
+    if (!refreshToken) throw new UnauthorizedException("refresh token을 수신하지 못했습니다.");
+
     const { sub, isOnboarding } = payload;
     if (!isOnboarding) throw new UnauthorizedException("이미 회원가입 절차를 완료한 사용자 입니다.");
 
