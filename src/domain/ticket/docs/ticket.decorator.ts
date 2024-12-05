307f7a2824b4f5dfd4e68baa11b71364d0ac56cd
liverforpresent <dailynsteady@gmail.com>
2024-12-06 00:59:13 +0900

    docs: swagger 수정

--- src/domain/ticket/docs/ticket.decorator.ts
+++ src/domain/ticket/docs/ticket.decorator.ts
@@ -1,5 +1,5 @@
 import { applyDecorators } from "@nestjs/common";
-import { ApiBody, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiParam } from "@nestjs/swagger";
+import { ApiBody, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiParam, ApiQuery } from "@nestjs/swagger";
 import { GetTicketResDto } from "../dtos/getTicket.dto";
 import { CreateTicketReqDto } from "../dtos/createTicket.dto";
 import { UpdateTicketReqDto } from "../dtos/updateTicket.dto";
@@ -18,7 +18,7 @@ export function Docs(endPoint: EndPoints) {
         description: "구매권 목록 조회. [{id, 이름, 가격}] 반환",
         summary: "구매권 목록 조회"
       }),
-      ApiParam({
+      ApiQuery({
         name: "studioId",
         type: Number,
         description: "스튜디오 고유 id"
