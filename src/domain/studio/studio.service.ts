01b961be8a6da51ed2eeca18deeb26e3cdc9df85
Na1gae <nalgae@korea.ac.kr>
2024-12-05 16:58:15 +0900

    Feat: Studio Info/Announcements

--- src/domain/studio/studio.service.ts
+++ src/domain/studio/studio.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { LectureGroup } from '../lecture/entities/lecture.group.entity';
 import { Studio } from './entities/studio.entity';
 import { Lecture } from '../lecture/entities/lecture.entity';
 import { Repository } from 'typeorm';
+import { GetLectureDto } from '../lecture/dtos/lecture.dto';
 
 @Injectable()
 export class StudioService {
@@ -11,25 +11,40 @@ export class StudioService {
         @InjectRepository(Lecture)
         private lectureRepository: Repository<Lecture>,
         @InjectRepository(Studio)
-        private studioRepository: Repository<Studio>,
-        @InjectRepository(LectureGroup)
-        private lectureGroupRepository: Repository<LectureGroup>
+        private studioRepository: Repository<Studio>
     ){}
 
     async getStudioInfo(id: number){
-        const stud = this.studioRepository.findOne({
+        const stud = await this.studioRepository.findOne({
             where: {id: id},
             relations: ['lectures']
         })
-        return stud
+        if(!stud) throw new NotFoundException
+
+        const lectures: GetLectureDto[] = stud.lectures.map((e) => ({
+            lectureId: e.id,
+            thumbnail: `${process.env.CLOUDFRONT_URL}/$${id}/${e.file.filename}`,
+            studioName: stud.name,
+            instructor: e.instructor,
+            description: e.description
+        }))
+
+        return {
+            name: stud.name,
+            description: stud.description,
+            policy: stud.policy,
+            lectures
+        }
     }
     
     async getStudioAnnouncement(id: number){
-        const stud = this.studioRepository.findOne({
+        const stud = await this.studioRepository.findOne({
             where: {id: id},
             relations: ['announcements']
         })
-        const announcements = (await stud).announcements
+
+        if(!stud) throw new NotFoundException
+        const announcements = stud.announcements.map(({studio,  ...rest}) => rest)
         return announcements
     }
 }
