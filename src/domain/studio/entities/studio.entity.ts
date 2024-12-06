9aa016dc210cae7df9075a3bb9965066a20bbf12
Na1gae <nalgae@korea.ac.kr>
2024-12-06 15:51:17 +0900

    Feat: Added Thumbnail on Studio

--- src/domain/studio/entities/studio.entity.ts
+++ src/domain/studio/entities/studio.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Announcement } from "./announcement.entity";
 import { Lecture } from "src/domain/lecture/entities/lecture.entity";
 import { User } from "src/domain/user/entities/user.entity";
@@ -29,4 +29,8 @@ export class Studio{
     
     @OneToMany(()=> Media, (media) => media.studio)
     medias: Media[]
+
+    @OneToOne(()=> Media, {nullable: true})
+    @JoinColumn()
+    thumbnail: Media
 }
